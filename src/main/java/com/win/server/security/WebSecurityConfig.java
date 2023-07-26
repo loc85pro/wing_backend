36e9996dd590905f7441c1b3d1058d08d43b5fbf
Phan Lộc <20120320@student.hcmus.edu.vn>
2023-07-26 13:39:12 +0700

    some

--- src/main/java/com/win/server/security/WebSecurityConfig.java
+++ src/main/java/com/win/server/security/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig  {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
