4d7e3dfa03d636c04564f416b35f759fcc090a9b
Phan Lộc <20120320@student.hcmus.edu.vn>
2023-07-28 14:41:55 +0700

    get all post by owner

--- src/main/java/com/win/server/controller/PostController.java
+++ src/main/java/com/win/server/controller/PostController.java
@@ -1,13 +1,15 @@
 package com.win.server.controller;
 
-import com.win.server.DTO.post.CreatePostRequest;
 import com.win.server.constant.PostPrivacy;
 import com.win.server.entity.PostEntity;
+import com.win.server.security.ContextUserManager;
 import com.win.server.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("post")
@@ -28,4 +30,10 @@ public class PostController {
         return null;
     }
 
+    @GetMapping("/me")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PostEntity> getOwnPost() {
+        String current_user_id = ContextUserManager.getUserId();
+        return postService.getPostByOwner(current_user_id);
+    }
 }
