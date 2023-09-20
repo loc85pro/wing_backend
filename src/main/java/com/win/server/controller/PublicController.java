17e057f5cc7cbf1f39fd6fc0db0e12850a73155a
pxl26 <20120320@student.hcmus.edu.vn>
2023-09-20 14:41:47 +0700

    lkhjdfklsjdhf

--- src/main/java/com/win/server/controller/PublicController.java
+++ src/main/java/com/win/server/controller/PublicController.java
@@ -91,6 +91,6 @@ public class PublicController {
     }
     @GetMapping("/test")
     public String testEndPoint() {
-        return "Hello bro nhe hehe (14:30)!!!";
+        return "Hello bro nhe hehe (14:41)!!!";
     }
 }
